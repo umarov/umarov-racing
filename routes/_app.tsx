fdd1494d4c0c5fe246491c41d89940555c0a913a
Muzafar Umarov <mumarov@invoca.com>
2023-09-11 16:53:06 -0400

    Refine home page

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,9 +6,19 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>umarov-racing</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Umarov Racing</title>
       </head>
-      <body>
+      <body style={{ fontFamily: "'Urbanist', sans-serif" }}>
         <Component />
       </body>
     </html>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,34 @@
 export default function Home() {
   return (
-    <div class="px-4 py-4 mx-auto">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center py-4">
-        <h1 class="text-3xl">Umarov Racing</h1>
+    <div class="container mx-auto">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center py-4 bg-[#db4f47]">
+        <h1 class="text-3xl font-bold	 text-gray-50">Umarov Racing</h1>
       </div>
 
-      <div class="flex flex-col bg-[#2f3dab] p-2 text-gray-50">
+      <div class="flex flex-col  p-2 text-gray-600">
         <p class="text-xl">
           My name is Muzafar Umarov
         </p>
+        <p class="text-xl">
+          I am a grassroots racer.
+        </p>
 
-        <div className="flex items-center justify-center ">
-          <p>
+        <p className="text-xl">
+          I compete in Time Trials with
+        </p>
+        <img class="bg-gray-50" src="./scca.png" />
+        <img class="bg-gray-50" src="./gridlife.png" />
+
+        <div className="flex items-center justify-center columns-2 py-2">
+          <p class="text-3xl text-[#2f3dab]">
             I drive a 2023 Hyundai Elantra N
           </p>
 
-          <img src="./elantran640.webp" alt="Elantra N" />
+          <img
+            src="./elantran640.webp"
+            alt="Elantra N"
+            style={{ width: "50%", height: "auto" }}
+          />
         </div>
       </div>
     </div>
