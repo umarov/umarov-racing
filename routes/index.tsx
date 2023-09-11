adb2433da674473f1b9ff7c0380203b817aaef96
Muzafar Umarov <mumarov@invoca.com>
2023-09-11 17:09:05 -0400

    More changes

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,23 +5,42 @@ export default function Home() {
         <h1 class="text-3xl font-bold	 text-gray-50">Umarov Racing</h1>
       </div>
 
-      <div class="flex flex-col  p-2 text-gray-600">
-        <p class="text-xl">
-          My name is Muzafar Umarov
-        </p>
-        <p class="text-xl">
-          I am a grassroots racer.
-        </p>
+      <div class="flex flex-col p-2 text-gray-600">
+        <div className="flex items-center columns-2 py-2">
+          <img
+            src="./vnshenandoah.webp"
+            alt="Elantra N"
+            style={{ width: "45%", height: "auto", alignSelf: "start" }}
+          />
+          <div class="pl-1">
+            <p class="text-xl">
+              My name is Muzafar Umarov
+            </p>
+            <p class="text-xl">
+              I have been doing track days since 2016.
+            </p>
+          </div>
+        </div>
 
-        <p className="text-xl">
-          I compete in Time Trials with
-        </p>
-        <img class="bg-gray-50" src="./scca.png" />
-        <img class="bg-gray-50" src="./gridlife.png" />
+        <div className="columns-1 rounded shadow-md p-2 flex flex-col items-center justify-center">
+          <p className="text-xl font-medium">
+            In 2023, I started to complete in Time Trials with
+          </p>
+          <img
+            class="bg-white"
+            style={{ height: "auto", width: "50%" }}
+            src="./scca.png"
+          />
+          <img
+            class="bg-white"
+            style={{ height: "auto", width: "50%" }}
+            src="./gridlife.png"
+          />
+        </div>
 
         <div className="flex items-center justify-center columns-2 py-2">
           <p class="text-3xl text-[#2f3dab]">
-            I drive a 2023 Hyundai Elantra N
+            I currently drive a 2023 Hyundai Elantra N
           </p>
 
           <img
