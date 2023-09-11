17974b4200c5ef69600dc590a1e52680ea38e2e5
Muzafar Umarov <mumarov@invoca.com>
2023-09-10 22:26:04 -0400

    Add simple link bio page

---
+++ routes/link-bio/index.tsx
@@ -0,0 +1,29 @@
+export default function LinkBio() {
+  return (
+    <div class="px-4 py-4 mx-auto">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center py-4">
+        <h1 class="text-3xl">Umarov Racing</h1>
+      </div>
+
+      <div class="flex flex-col bg-[#2f3dab] p-2 text-gray-50">
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/umarov_racing/">
+              Instagram Page
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/umarovracing">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/@umarovracing">
+              YouTube
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
